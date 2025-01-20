e97fe9d87a12049355e607c01ebf09ebf6a12954
Hesham Seliem <heshamseliem9@gmail.com>
2025-01-20 14:31:21 +0200

    secend commit

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+static const String routeName = '/intro';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: SafeArea(
+
+        minimum: const EdgeInsets.symmetric(horizontal: 16,vertical: 25),
+        child: Center(
+          child: Column(
+
+            children: [
+              //Image.asset("assets/images/header.png"),
+              const SizedBox(height: 28,),
+              Image.asset("assets/images/welcome.png"),
+               const SizedBox(height: 28,),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                 children: [
+                   Text("Personalize Your Experience",style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xff5669FF),
+                      fontWeight: FontWeight.w700
+                    )
+                   ),
+                   
+                 ],
+
+               ),
+               const SizedBox(height: 28,),
+                const Text("Choose your preferred theme and language to get started with a comfortable, tailored experience that suits your style.",
+                style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff1C1C1C),
+                      fontWeight: FontWeight.w500
+                    )
+                  
+                   ),
+                   const SizedBox(height: 28,),
+                  const  Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                 children: [
+                   Text("Language",style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xff5669FF),
+                      fontWeight: FontWeight.w700
+                    )
+                   
+                   ),
+                 ],
+
+               ),
+               const SizedBox(height: 16,),
+               const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                 children: [
+                   Text("Theme",style:
+                 TextStyle(
+                      fontSize: 20,
+                      color: Color(0xff5669FF),
+                      fontWeight: FontWeight.w700
+                    )
+                   ),
+                   
+                 ],
+
+               ),
+               const SizedBox(height: 28,),
+               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:const Color(0xff5669FF),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding:const EdgeInsets.symmetric(horizontal: 135,vertical: 15)
+                ),
+                onPressed: (){},
+                child: const Text("Let’s Start",style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white
+                ),)
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
